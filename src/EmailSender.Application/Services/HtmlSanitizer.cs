32dcd81d3250ca82590e901e191c67bd8deb8f34
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2025-07-24 20:30:23 -0300

    Adjuste sanitizer to keep css with no malicious script (XSS Protection)

--- src/EmailSender.Application/Services/HtmlSanitizer.cs
+++ src/EmailSender.Application/Services/HtmlSanitizer.cs
@@ -8,6 +8,22 @@ namespace EmailSender.Application.Services
         public HtmlSanitizerService()
         {
             _sanitizer = new HtmlSanitizer();
+
+            #region Filters
+            _sanitizer.FilterUrl += (s, e) =>
+            {
+                if (e.OriginalUrl.StartsWith("javascript:", StringComparison.OrdinalIgnoreCase))
+                    e.SanitizedUrl = null;
+            };
+            #endregion
+
+            #region Allowed Tags
+            _sanitizer.AllowedTags.Add("style");
+            _sanitizer.AllowedAttributes.Add("style");
+            _sanitizer.AllowedAttributes.Add("class");
+            _sanitizer.AllowedAttributes.Add("id");
+            _sanitizer.AllowedTags.Add("svg");
+            #endregion
         }
 
         public string Sanitize(string html) => _sanitizer.Sanitize(html);
